67bf3137fabb2f320a5e225329b9c19d57ed5575
ben <bin.go2011@yahoo.com>
2020-03-29 11:37:53 +0800

    _GoString_  c macro

--- 2.1.6/main.go
+++ 2.1.6/main.go
@@ -1,14 +1,14 @@
 package main
 
-//void SayHello(char* s);
+//void SayHello(_GoString_ s);
 import "C"
 import "fmt"
 
 func main() {
-	C.SayHello(C.CString("Hello, World\n"))
+	C.SayHello("Hello, World\n")
 }
 
 //export SayHello
-func SayHello(s *C.char) {
-	fmt.Print(C.GoString(s))
+func SayHello(s string) {
+	fmt.Print(s)
 }
